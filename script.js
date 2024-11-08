d98692e0babaab51d10c3f8fdd35be25e2b057d5
Paweł Czajczyk <czajczykp@gmail.com>
2024-11-08 23:51:05 +0100

    slide animation + video handler

--- script.js
+++ script.js
@@ -8,43 +8,61 @@ menuBtn.addEventListener('click', () => {
 const slide = document.querySelector('.slides')
 const slides = document.querySelectorAll('.slide');
 const btns = document.querySelectorAll('.slides button');
-const displaySlide = document.getElementById('displaySlide')
+const displaySlide = document.getElementsByClassName('displaySlide')
+const displayImg = document.getElementById('displayImg')
+const displayVideo = document.getElementById('displayVideo')
 let slideId = 0;
 let intervalId;
 
 
 window.addEventListener("DOMContentLoaded", function () {
     showSlide(0);
+    startSlides();
+})
+
+for (let i = 0; i < displaySlide.length; i++){
+    displaySlide[i].addEventListener("mouseenter", () => clearInterval(intervalId))
+    displaySlide[i].addEventListener("mouseleave", () => startSlides())
+}
+
+function startSlides(){
     intervalId = setInterval(() => {
         nextSlide()
     }, 5000)
-})
+}
 
 function showSlide(slideIdIn) {
+    clearInterval(intervalId);
+    startSlides();
     if (slideIdIn < 0) slideIdIn = slides.length - 1;
     if (slideIdIn >= slides.length) slideIdIn = 0;
+    slides[slideId].classList.remove("showSlide");
     slides[slideIdIn].classList.add("showSlide");
-    displaySlide.src = slides[slideIdIn].src
     slideId = slideIdIn
+    if (slides[slideIdIn].nodeName === "VIDEO"){
+        displayImg.style.display = 'none'
+        displayVideo.style.display = 'block'
+        displaySlide[1].alt = slides[slideIdIn].alt
+        displaySlide[1].src = slides[slideIdIn].src
+    }
+    else {
+        displayImg.style.display = 'block'
+        displayVideo.style.display = 'none'
+        displaySlide[0].alt = slides[slideIdIn].alt
+        displaySlide[0].src = slides[slideIdIn].src
+        displaySlide[1].removeAttribute('src')
+
+    }
 }
 
 function nextSlide() {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-        nextSlide()
-    }, 5000);
-    console.log(slideId)
     slides[slideId].classList.remove("showSlide");
-    showSlide(++slideId);
+    showSlide(slideId + 1);
 }
 
 function prevSlide() {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-        nextSlide()
-    }, 5000);
     slides[slideId].classList.remove("showSlide");
-    showSlide(--slideId);
+    showSlide(slideId - 1);
 }
 
 
