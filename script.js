51657d7bb80887a6604f1eaeafe71e28ad7297a6
Paweł Czajczyk <czajczykp@gmail.com>
2024-11-09 18:56:52 +0100

    navbar menu toggle

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const menuBtn = document.getElementById('menu-toggle');
-const menu = document.getElementById('header--nav');
+const options = Array.from(document.querySelectorAll(".options"))
+const menuUl = document.querySelector('#header--nav ul')
+let menuActive = false;
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
+function changeVis() {
+    menuActive = !menuActive
+    options.forEach(option => {
+        option.classList.toggle('active')
+    })
+}
+
+menuBtn.addEventListener('click', () => changeVis())
+
+
+window.addEventListener('click', (e) => {
+    const {left, right, top, bottom} = menuUl.getBoundingClientRect();
+    // console.log(e.clientX >= left)
+    // console.log(e.clientX <= right)
+    // console.log(e.clientY <= top)
+    // console.log(e.clientY >= bottom)
+    // console.log('-------------------')
+    if (menuActive && !(e.clientX >= left && e.clientX <= right && e.clientY >= top && e.clientY <= bottom)) {
+        changeVis()
+    }
 })
